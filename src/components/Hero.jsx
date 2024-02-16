e4574e4356e3e81d397f1d41aeb832aaadabd423
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-02-16 06:08:32 -0600

    Add framer-motion and react-responsive dependencies, and apply motion animations to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,60 @@
 import React from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import {motion} from 'framer-motion'
+
 
 function Hero() {
   return (
-    <div className="w-full px-4 lg:px-0 2xl:px-14 h-screen lg:h-screen 2xl:h-[900px] flex flex-col lg:flex-row lg:pl-8">
+    <div className="w-full px-4 lg:px-0 2xl:px-14 h-screen  xl:h-[900px] flex flex-col gap-[64px] lg:gap-[0px] lg:flex-row lg:pl-8">
       <div className="w-full h-full flex flex-col justify-center items-center">
         <div className="w-full flex flex-col gap-[24px]">
-          <h1 className="font-header font-bold leading-[48px] lg:leading-[67px] text-[40px] lg:text-[56px] w-full lg:w-[95%] xl:w-[576px] text-center lg:text-start">
+          <motion.h1
+           initial={{ y: -100 }}
+           animate={{ y: 0 }}
+           transition={{ duration: 2, ease: "easeOut" }}
+           className="font-header font-bold leading-[48px] lg:leading-[67px] text-[40px] lg:text-[56px] w-full lg:w-[95%] xl:w-[576px] text-center lg:text-start">
             Descubra el poder de nuestras soluciones de limpieza
-          </h1>
-          <p className="w-full lg:w-[95%] xl:w-[576px] font-paragraph text-[16px] lg:text-[18px] font-normal leading-[24px] lg:leading-[27px] text-center lg:text-start ">
+          </motion.h1>
+          <motion.div className="w-full h-full"
+          initial={{ y: 100 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 2, ease: "easeOut" }}>
+            
+          <p className="w-full lg:w-[95%] xl:w-[576px] font-paragraph text-[16px] lg:text-[18px] font-normal leading-[24px] lg:leading-[27px] text-center lg:text-start mb-4"
+           
+          >
             Experimenta la más alta calidad de trapo industrial en el mercado,
             que harán la diferencia en tu negocio gracias a nuestra eficacia y
             versatilidad mejorando la productividad y eficiencia de tus
             instalaciones.
           </p>
           <div className="w-full  flex flex-row justify-center lg:justify-start items-center gap-[8px]">
-            <button className="bg-brand-primary shadow-brand w-[139px] rounded-[10px] text-white px-[24px] py-[12px] hover:scale-105 transition-all duration-300">
+            <motion.button 
+              className="bg-brand-primary shadow-brand w-[139px] rounded-[10px] text-white px-[24px] py-[12px] hover:scale-105 transition-all duration-300"
+              
+            >
               Descubre
-            </button>
-            <button className="bg-brand-secondary shadow-brand text-white rounded-[10px] w-[139px] px-[24px] py-[12px] hover:scale-105 transition-all duration-300">
+            </motion.button>
+            <motion.button 
+              className="bg-brand-secondary shadow-brand text-white rounded-[10px] w-[139px] px-[24px] py-[12px] hover:scale-105 transition-all duration-300"
+            
+            >
               Contáctanos
-            </button>
+            </motion.button>
+            
           </div>
+          </motion.div>
         </div>
       </div>
       <div className="w-full h-full flex flex-row overflow-hidden gap-2">
-        <div className="w-full h-full  flex flex-col gap-2 transform -translate-y-[77px] lg:-translate-y-[130px] ">
+        <motion.div
+         className="w-full h-full  flex flex-col gap-2 transform -translate-y-[77px] lg:-translate-y-[130px]"
+ 
+  initial={{ y: 100 }}
+  animate={{ y: -130 }}
+  transition={{ duration: 2, ease: "easeOut" }}
+         
+         >
 
           <div className="w-full h-full bg-gray-600 rounded-[20px] lg:rounded-[40px]">
             <div className="w-[160px] md:h-[340px] md:w-[150px] lg:w-[320px] h-[170px] lg:h-[340px]"/>
@@ -39,8 +67,12 @@ function Hero() {
               
             </div>
           
-        </div>
-        <div className="h-full w-full flex flex-col  gap-2 ">
+        </motion.div>
+        <motion.div className="h-full w-full flex flex-col  gap-2 "
+        initial={{ y: -130 }}
+        animate={{ y: 0 }}
+        transition={{ duration: 2, ease: "easeOut" }}
+        >
           <div className="w-full h-full bg-gray-600 rounded-[20px] lg:rounded-[40px]">
             <div className="w-[160px] md:h-[340px] md:w-[150px] lg:w-[320px] h-[170px] lg:h-[340px]"></div>
           </div>
@@ -50,7 +82,7 @@ function Hero() {
           <div className="w-full h-full bg-gray-600 rounded-[20px] lg:rounded-[40px]">
             <div className="w-[160px] md:h-[340px] md:w-[150px] lg:w-[320px] h-[170px] lg:h-[340px]"></div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
