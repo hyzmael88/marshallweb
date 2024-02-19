9b2c9d9de40be2ede802c22fec936a06cacf8fd9
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-02-18 22:17:24 -0600

    Refactor Hero component layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -102,7 +102,9 @@ function Hero() {
               <div className="w-[160px] md:h-[340px] md:w-[150px] lg:w-[320px] h-[170px] lg:h-[340px]"/>
             </div>
             <div className="w-full h-full bg-gray-600 rounded-[20px] lg:rounded-[40px]">
-              <div className="w-[160px] md:h-[340px] md:w-[150px] lg:w-[320px] h-[170px] lg:h-[340px]"/>
+              <div
+              
+              className="w-[160px] md:h-[340px] md:w-[150px] lg:w-[320px] h-[170px] lg:h-[340px]"/>
               
             </div>
           
