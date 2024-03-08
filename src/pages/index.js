dcb2ad11c164d70ffe7b8e08367122dd6841a330
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-08 04:54:25 -0600

    Add meta tags and title to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
@@ -15,21 +16,33 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className='w-full h-full max-w-[1444px] mx-auto relative min-w-[354px]'>
-     <Navbar/>
-     <Hero/>
-     <Acerca/>
-     <HotspotImage/>
-     <Proceso/>
-     <Productos/>
-     <Testimonios/>
-     <Contacto/>
-     <Footer/>
-     <Jaizmora/>
-     <a href="https://wa.me/+525517935266?text=Hola!%20me%20podrías%20dar%20mas%20información?" 
-        style={{position: 'fixed', bottom: '25px', right: '25px', padding: '10px', backgroundColor: '#25D366', color: 'white', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '80px', height: '80px', textDecoration: 'none'}}>
-       <img src="/images/whatsapp.svg" alt="WhatsApp" style={{width: '48px', height: '48px'}}/>
-     </a>
-    </main>
-   )
+    <>
+      <Head>
+        <title>Productos Industriales Marshall</title>
+        <meta name="description" content="Experimenta la más alta calidad de trapo industrial en el mercado, que harán la diferencia en tu negocio gracias a nuestra eficacia y versatilidad mejorando la productividad y eficiencia de tus instalaciones." />
+        <meta name="keywords" content="trapo, trapo industrial, trapo tipo camiseta, trapo unido, trapo color" />
+        <meta property="og:title" content="Productos Industriales Marshall" />
+        <meta property="og:description" content="Experimenta la más alta calidad de trapo industrial en el mercado, que harán la diferencia en tu negocio gracias a nuestra eficacia y versatilidad mejorando la productividad y eficiencia de tus instalaciones." />
+        <meta property="og:image" content="/images/ogimage.png" />
+        <meta property="og:url" content="www.productosindustrialesmarshall.com" />
+        <meta name="twitter:card" content="/images/ogimage.png" />
+      </Head>
+      <main className='w-full h-full max-w-[1444px] mx-auto relative min-w-[354px]'>
+        <Navbar/>
+        <Hero/>
+        <Acerca/>
+        <HotspotImage/>
+        <Proceso/>
+        <Productos/>
+        <Testimonios/>
+        <Contacto/>
+        <Footer/>
+        <Jaizmora/>
+        <a href="https://wa.me/+525517935266?text=Hola!%20me%20podrías%20dar%20mas%20información?" 
+          style={{position: 'fixed', bottom: '25px', right: '25px', padding: '10px', backgroundColor: '#25D366', color: 'white', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '80px', height: '80px', textDecoration: 'none'}}>
+          <img src="/images/whatsapp.svg" alt="WhatsApp" style={{width: '48px', height: '48px'}}/>
+        </a>
+      </main>
+    </>
+  )
 }
