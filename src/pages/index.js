f74d45367ce7a98d622802b61194c9204c35ab39
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-27 20:47:10 -0600

    Add Google Analytics tracking code

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ import Contacto from '@/components/Contacto'
 import Footer from '@/components/Footer'
 import Jaizmora from '@/components/Jaizmora'
 import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,17 +31,7 @@ export default function Home() {
         <meta name="twitter:card" content="/images/ogimage.png" />
       </Head>
       <main className='w-full h-full max-w-[1444px] mx-auto relative min-w-[354px] overflow-hidden'>
-      <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TKX27XQM');`,
-            }}
-          />
+      <GoogleAnalytics gaId="GTM-TKX27XQM" />
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKX27XQM"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Navbar/>
