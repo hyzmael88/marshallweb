bf9a3e093bc29828ae79723c2afb10b7519181a0
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-03-27 21:15:29 -0600

    Add Google Tag Manager event tracking for WhatsApp button

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import Contacto from '@/components/Contacto'
 import Footer from '@/components/Footer'
 import Jaizmora from '@/components/Jaizmora'
 import Script from 'next/script';
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager, sendGTMEvent } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -44,7 +44,9 @@ export default function Home() {
         <Jaizmora/>
         <a href="https://wa.me/+525517935266?text=Hola!%20me%20podrías%20dar%20mas%20información?" 
         className='animate-pulse transition-all duration-700 hover:animate-none hover:scale-110 '
-          style={{position: 'fixed', bottom: '25px', right: '25px', padding: '10px', backgroundColor: '#25D366', color: 'white', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '80px', height: '80px', textDecoration: 'none'}}>
+          style={{position: 'fixed', bottom: '25px', right: '25px', padding: '10px', backgroundColor: '#25D366', color: 'white', borderRadius: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center', width: '80px', height: '80px', textDecoration: 'none'}}
+          onClick={sendGTMEvent('whatsapp', 'click', 'whatsapp')}
+          >
           <Image src="/images/whatsapp.svg" alt="WhatsApp" width={100} height={100}
           
           style={{width: '48px', height: '48px'}}/>
